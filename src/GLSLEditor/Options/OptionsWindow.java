0ba0a5c1b0113e47962dc2256aece39c3bd0c209
Heikki <heikki.simojoki@gmail.com>
2015-08-04 19:05:53 +0300

    Just before leaving for cabin

--- src/GLSLEditor/Options/OptionsWindow.java
+++ src/GLSLEditor/Options/OptionsWindow.java
@@ -6,20 +6,25 @@ import GLSLEditor.Layouts.OptionsLayout.OptionsLayoutController;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OptionsWindow {
     private static Editor editor;
+    private static List<Label> tabs;
+    private static Label selected;
 
     public static void init(Editor editor){
         OptionsWindow.editor = editor;
     }
 
     public static void show(){
-
+        tabs = new ArrayList<Label>();
 
         Stage optionsWindow = new Stage();
         optionsWindow.initModality(Modality.APPLICATION_MODAL);
@@ -38,35 +43,42 @@ public class OptionsWindow {
 
         optionsScene = new Scene(root, 500, 400);
 
-        optionsScene.getStylesheets().add(editor.getClass().getResource("Layouts/MainLayout/MainLayoutStyle.css").toExternalForm());
+        optionsScene.getStylesheets().add(editor.getClass().getResource("Layouts/OptionsLayout/OptionsLayoutStyle.css").toExternalForm());
 
         optionsWindow.setScene(optionsScene);
 
 
+        tabs.add(c.GeneralOptionsTab);
+        tabs.add(c.EditorOptionsTab);
+        tabs.add(c.HotkeyOptionsTab);
 
 
+        for(Label l : tabs){
 
+            l.setOnMouseEntered(e -> {if(selected != l)l.setId("OptionsTab_hover");});
+            l.setOnMouseExited(e -> {if (selected != l) l.setId("OptionsTab_idle");});
+            l.setOnMouseClicked(e -> {if(selected != l) select(l);});
+            l.setId("OptionsTab_idle");
+        }
 
-
-
-
-
-
-
+        select(c.GeneralOptionsTab);
 
         optionsWindow.show();
 
 
 
-
-
-
-
-
-
-
-
     }
+
+
+    private static void select(Label l){
+        if(selected != null) selected.setId("OptionsTab_idle");
+        selected = l;
+        selected.setId("OptionsTab_selected");
+        System.out.println(l.getText());
+    }
+
+
+
 
 
 }
